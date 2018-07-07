0636171ef157bcf90e2d4434088966710b615372
Igor Franca <lee12rock@gmail.com>
2018-07-06 21:51:08 -0300

    Modificado para virar uma função main, dentro de server.js

--- encontro6/server.js
+++ encontro6/server.js
@@ -2,15 +2,19 @@ const dotenv = require('dotenv');
 
 const app = require('./src/app');
 
-switch(process.env.NODE_ENV) {
-    case 'PRODUCAO':
-        dotenv.config({path: './config/.env.prod'});
-        break;
-    case 'DESENVOLVIMENTO':
-        dotenv.config({path: './config/.env.dev'});
-        break;
-    default:
-        dotenv.config({path: './config/.env.dev'});
+function main() {
+    switch(process.env.NODE_ENV) {
+        case 'PRODUCAO':
+            dotenv.config({path: './config/.env.prod'});
+            break;
+        case 'DESENVOLVIMENTO':
+            dotenv.config({path: './config/.env.dev'});
+            break;
+        default:
+            dotenv.config({path: './config/.env.dev'});
+    }
+
+    app();
 }
 
-app();
+main();
