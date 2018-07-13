c333d4373090c66375f1e77d5b447e95262662ca
Igor Franca <lee12rock@gmail.com>
2018-07-13 19:50:00 -0300

    Adicionado identação!

--- encontro6/server.js
+++ encontro6/server.js
@@ -5,13 +5,13 @@ const app = require('./src/app');
 function main() {
     switch(process.env.NODE_ENV) {
         case 'PRODUCAO':
-        dotenv.config({path: './config/.env.prod'});
-        break;
+            dotenv.config({path: './config/.env.prod'});
+            break;
         case 'DESENVOLVIMENTO':
-        dotenv.config({path: './config/.env.dev'});
-        break;
+            dotenv.config({path: './config/.env.dev'});
+            break;
         default:
-        dotenv.config({path: './config/.env.dev'});
+            dotenv.config({path: './config/.env.dev'});
     }
     
     app();
